1e3dc8c8fe2dc97855fdee21ada1a57bc94e0a72
Adi-070 <mishraaditya760@gmail.com>
2024-06-26 12:19:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Data = require('./models/Data');
+const app = express();
+const port = 3001;
+
+const uri = "mongodb+srv://mishraaditya760:RJSMwAowfZSwY7D0@cluster0.jakbfab.mongodb.net/<DATABASE>?retryWrites=true&w=majority";
+
+mongoose.connect(uri)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use(cors());
+
+app.get('/data', async (req, res) => {
+    try {
+        const data = await Data.find();
+        res.json(data);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error fetching data from MongoDB');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
